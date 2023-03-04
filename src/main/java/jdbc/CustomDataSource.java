4fdab5d1badb4480ce4b1faaa280e82ddebc2475
dailar <dailarse00@gmail.com>
2023-03-04 16:34:46 +0300

    done?

--- src/main/java/jdbc/CustomDataSource.java
+++ src/main/java/jdbc/CustomDataSource.java
@@ -1,25 +1,106 @@
 package jdbc;
 
-import javax.sql.DataSource;
-
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Logger;
 
 @Getter
 @Setter
 public class CustomDataSource implements DataSource {
-    private static volatile CustomDataSource instance;
+    private static volatile CustomDataSource instance = new CustomDataSource();
     private final String driver;
     private final String url;
     private final String name;
     private final String password;
+    private final CustomConnector connector = new CustomConnector();
 
-    private CustomDataSource(String driver, String url, String password, String name) {
+    private CustomDataSource() {
+        Properties properties;
+        String fileName = "app.properties";
+        try {
+            FileInputStream fileInputStream = new FileInputStream(fileName);
+            properties = new Properties();
+            properties.load(fileInputStream);
+        } catch (IOException e) {
+            throw new RuntimeException("can't find " + fileName);
+        }
+
+        url = properties.getProperty("postgres.url");
+        name = properties.getProperty("postgres.name");
+        password = properties.getProperty("postgres.password");
+        driver = properties.getProperty("postgres.driver");
+        init();
+    }
+
+    private void init() {
+        try {
+            PreparedStatement preparedStatement = getConnection().prepareStatement("drop database if exists myfirstdb; create database myfirstdb; " +
+                    "CREATE TABLE if not exists myusers (id bigint unique GENERATED ALWAYS AS IDENTITY, firstname text,lastname text,age smallint);");
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     public static CustomDataSource getInstance() {
-
         return instance;
     }
 
+
+    @Override
+    public Connection getConnection() throws SQLException {
+        return connector.getConnection(url);
+    }
+
+    @Override
+    public Connection getConnection(String username, String password) throws SQLException {
+        return connector.getConnection(url, username, password);
+    }
+
+    @Override
+    public PrintWriter getLogWriter() {
+        return null;
+    }
+
+    @Override
+    public void setLogWriter(PrintWriter out) {
+    }
+
+    @Override
+    public void setLoginTimeout(int seconds) {
+
+    }
+
+    @Override
+    public int getLoginTimeout() {
+        return 0;
+    }
+
+    @Override
+    public Logger getParentLogger() {
+        return null;
+    }
+
+    @Override
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+        if (iface.isAssignableFrom(getClass())) {
+            return iface.cast(this);
+        }
+        throw new SQLException("Cannot unwrap to " + iface.getName());
+    }
+
+    @Override
+    public boolean isWrapperFor(Class<?> iface) {
+        return iface.isAssignableFrom(getClass());
+    }
 }
--- src/main/java/jdbc/SimpleJDBCRepository.java
+++ src/main/java/jdbc/SimpleJDBCRepository.java
@@ -6,9 +6,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.Statement;
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -17,32 +17,100 @@ import java.util.List;
 @NoArgsConstructor
 public class SimpleJDBCRepository {
 
-    private Connection connection = null;
-    private PreparedStatement ps = null;
-    private Statement st = null;
+    private DataSource dataSource = CustomDataSource.getInstance();
 
-    private static final String createUserSQL = "";
-    private static final String updateUserSQL = "";
-    private static final String deleteUser = "";
-    private static final String findUserByIdSQL = "";
-    private static final String findUserByNameSQL = "";
-    private static final String findAllUserSQL = "";
+    private static final String createUserSQL = "insert into myusers (firstname,lastname,age) values(?,?,?) returning id";
+    private static final String updateUserSQL = "update myusers set firstname=?,lastname=?,age=? where id=?";
+    private static final String deleteUser = "delete from myusers where id=?";
+    private static final String findUserByIdSQL = "select * from myusers where id=?";
+    private static final String findUserByNameSQL = "select * from myusers where firstname like ?";
+    private static final String findAllUserSQL = "select * from myusers";
 
-    public Long createUser() {
+    public Long createUser(User user) {
+        long id = 0;
+        try (Connection connection = dataSource.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(createUserSQL);
+            preparedStatement.setString(1, user.getFirstName());
+            preparedStatement.setString(2, user.getLastName());
+            preparedStatement.setInt(3, user.getAge());
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            id = resultSet.getLong(1);
+        } catch (SQLException ignored) {
+        }
+        return id;
     }
 
     public User findUserById(Long userId) {
+        try (Connection connection = dataSource.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(findUserByIdSQL);
+            preparedStatement.setLong(1, userId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return parseUser(resultSet);
+            }
+        } catch (SQLException ignored) {
+        }
+        return null;
     }
 
     public User findUserByName(String userName) {
+        try (Connection connection = dataSource.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(findUserByNameSQL);
+            preparedStatement.setString(1, userName);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return parseUser(resultSet);
+            }
+        } catch (SQLException ignored) {
+        }
+        return null;
     }
 
     public List<User> findAllUser() {
+        List<User> users = new ArrayList<>();
+        try (Connection connection = dataSource.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(findAllUserSQL);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                User user = parseUser(resultSet);
+                users.add(user);
+            }
+        } catch (SQLException ignored) {
+        }
+        return users;
     }
 
-    public User updateUser() {
+    public User updateUser(User user) {
+        try (Connection connection = dataSource.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(updateUserSQL);
+            preparedStatement.setString(1, user.getFirstName());
+            preparedStatement.setString(2, user.getLastName());
+            preparedStatement.setInt(3, user.getAge());
+            preparedStatement.setLong(4, user.getId());
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException ignored) {
+        }
+        return findUserById(user.getId());
     }
 
     private void deleteUser(Long userId) {
+        try (Connection connection = dataSource.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(deleteUser);
+            preparedStatement.setLong(1, userId);
+            preparedStatement.execute();
+        } catch (SQLException ignored) {
+        }
+    }
+
+
+    private static User parseUser(ResultSet resultSet) throws SQLException {
+        long id = resultSet.getLong(1);
+        String firstName = resultSet.getString(2);
+        String lastName = resultSet.getString(3);
+        int age = resultSet.getInt(4);
+        return new User(id, firstName, lastName, age);
     }
 }
